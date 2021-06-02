e9d0496305c498a41ad7f713c3b3a0e26a819e2f
MrZloHex <zlo.alex.it@gmail.com>
2021-06-02 11:31:02 +0300

    cou

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,27 @@
-enum Enum {
-    Enum1,
-    Enum2,
-}
-struct Structura {
-    value_i32: i32,
-    value_boolean: bool,
-}
-struct TupleStructura(f64, f32, i8, u32, char);
-fn function_enum1() -> Enum {
-    Enum::Enum1
-}
-fn function_enum2() -> Enum {
-    Enum::Enum2
-}
-fn f2() -> Structura {
-    Structura {
-        value_i32: 32,
-        value_boolean: true,
-    }
-}
-fn f3() -> TupleStructura {
-    TupleStructura(4.7, 2., 3, 5, 'w')
-}
-fn f4() -> [u8; 4] {
-    [7, 2, 0, 19]
-}
-fn f5() -> Vec<u8> {
-    vec![120]
+
+struct CPU {
+    // programme counter
+    PC: u16,
+    // stack pointer
+    SP: u16,
+
+    // accumulator
+    A: u8,
+    // 8-bit registers
+    X: u8,
+    Y: u8,
+
+    // status flags
+    C: bool,
+    Z: bool,
+    I: bool,
+    D: bool,
+    B: bool,
+    V: bool,
+    N: bool
 }
 
+
 fn main() {
-    println!(
-        "{} {}",
-        match function_enum1() {
-            Enum::Enum1 => 10,
-            Enum::Enum2 => 20,
-            
-        },
-        match function_enum2() {
-            Enum::Enum1 => true,
-            Enum::Enum2 => false,
-            
-        }
-    );
-    println!("{} ", f2().value_i32);
-    println!("{} ", f2().value_boolean);
-    println!("{} ", f3().0);
-    println!("{} ", f4()[0]);
-    println!("{} ", f5()[0]);
+    let _cpu: CPU = CPU {PC: 0, SP: 0, A: 0, X: 0, Y: 0, C: false, Z: false, I: false, D: false, B: false, V: false, N: false};
 }
