10def999fdf3efe784bdfbdef481702e3344ac73
MrZloHex <zlo.alex.it@gmail.com>
2021-06-02 13:13:48 +0300

    mem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-const MAX_MEM: usize = 1024 * 64;
+const MAX_MEM: usize = 1024 * 64; 
 
 struct CPU {
     // programme counter
@@ -23,28 +23,34 @@ struct CPU {
 }
 
 impl CPU {
-    fn reset(&mut self) -> () {
+    fn reset(&mut self, mem: &mut MEM) -> () {
         self.r_pc = 0xFF_FC;
         self.r_sp = 0x01_00;
         self.f_d = false;
+        mem.initialize();
     }
 }
 
+
+
 struct MEM {
-    data: [u8; MAX_MEM],
-
-
+    data: [u8; MAX_MEM]
 }
 
 impl MEM {
-
+    fn initialize(&mut self) -> () {
+        for byte in self.data.iter_mut() {
+            *byte = 0;
+        }
+    }
 }
+
 
 
 fn main() {
     let mut cpu: CPU = CPU {r_pc: 0, r_sp: 0, r_a: 0, r_x: 0, r_y: 0,
                             f_c: false, f_z: false, f_i: false, f_d: false, f_b: false, f_v: false, f_n: false};
 
-    let mut mem: MEM = MEM {data: [0, MAX_MEM]};
-    cpu.reset();
+    let mut mem: MEM = MEM {data: [0; MAX_MEM]};
+    cpu.reset(&mut mem);
 }
