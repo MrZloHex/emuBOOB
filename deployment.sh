a66696cfcab0e8071fbe6c728caf11767a3e9152
MrZloHex <zlo.alex.it@gmail.com>
2021-06-16 13:49:41 +0300

    TRY DEPLY

--- deployment.sh
+++ deployment.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 
+NAME="emuBOOB"
+
+load() {
+	while [ 1 ]
+	do
+		echo >&6 -ne "."
+		sleep 0.5
+	done
+}
+
 compile() {
 	echo >&6 -n "Compiling"
 
 	load &
 	PID=$!
+	
+	cargo build --release
+
 	echo >&6 ""
 
 	kill $PID
 }
 
-main() {
-	exec 6>&1 >/dev/null
-	
-	compile
+check_exec() {
+	if [ -f "./target/release/$NAME" ]
+	then
+		echo >&6 "Compiling finished succesfully"
+	else
+		echo >&6 "Compiling failed"
+	fi
 }
 
-# FOR ON ENTRY ROOT PASSWORD
-sudo ls
+main() {
+	exec 6>&1 >/dev/null
+	exec 2>/dev/null
+
+	sudo ls
+
+	compile
+	check_exec
+
+
+}
 
 main $@
